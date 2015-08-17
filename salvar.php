a7e0acf8b4e80a1d11cf1ba8ddecae045dc0b00f
GugaSevero <guga_severo@hotmail.com>
2015-08-17 16:14:28 -0300

    Create salvar.php

---
+++ salvar.php
@@ -0,0 +1,10 @@
+<?php
+$nome = $_POST['nome']; 
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+echo 'foi';
+
+//$ins = mysql_query("INSERT INTO users (name, email, pass) VALUES (NULL, '$nome', '$email', '$senha')");
+//$ins = "INSERT INTO users (name, email, pass) VALUES (NULL, '$nome', '$email', '$senha')";
+?>

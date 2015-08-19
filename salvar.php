32e9210bca476acdbf050a468441d24b580ff771
GugaSevero <guga_severo@hotmail.com>
2015-08-19 14:55:17 -0300

    Update salvar.php

--- salvar.php
+++ salvar.php
@@ -1,10 +1,15 @@
 <?php
+
 $nome = $_POST['nome']; 
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 
-echo 'foi';
+echo json_encode( array('status' => 1, 'msg' => 'Cadastro efetuado com sucesso!'));
+/*
+$con = mysql_connect('localhost', 'root', '');
+mysql_select_db('angularDB', $con);
 
 //$ins = mysql_query("INSERT INTO users (name, email, pass) VALUES (NULL, '$nome', '$email', '$senha')");
-//$ins = "INSERT INTO users (name, email, pass) VALUES (NULL, '$nome', '$email', '$senha')";
+$ins = "INSERT INTO users (name, email, pass) VALUES (NULL, '$nome', '$email', '$senha')";
+echo $ins;*/
 ?>
